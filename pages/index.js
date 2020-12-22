b7884367e60b9a0ca505d31dc2f217bf54ecaf3c
surumebeer <06shuji09@gmail.com>
2020-12-23 07:36:02 +0900

    2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+import { fetchGitHubIssues } from "./../lib/api.js";
+
+function HomePage({ posts }) {
+  return (
+    <div>
+      <ul>
+        {posts.map((x, i) => (
+          <li key={i}>{x.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await fetchGitHubIssues();
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
 
 export default HomePage;
